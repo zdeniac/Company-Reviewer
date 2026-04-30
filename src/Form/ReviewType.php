4da2c248c983f1d76428ac2e743cd46549cc1349
József Zdenyák <zdenyakj@gmail.com>
2026-04-30 16:51:01 +0200

    Added star rating

--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Review;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,7 @@ class ReviewType extends AbstractType
     {
         $builder
             ->add('companyName')
-            ->add('rating')
+            ->add('rating', HiddenType::class)
             ->add('reviewText')
             ->add('authorEmail');
     }
